763cc92b2c859c7e182e28c465802791a5d78af4
ng <naoufal.gaffa@gmail.com>
2020-12-23 22:47:18 +0100

    finalizing front local example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 const token =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkVMIEdBRkEgTkFPVUZBTCIsImFkbWluIjp0cnVlLCJqdGkiOiIxOGI2ZTdjMS00NzJiLTQ5NDctYTAyYS1hZmU5YzA3ZjdmNmQiLCJpYXQiOjE2MDg3NTE2NTEsImV4cCI6MTYwODc1NjEzOX0.Kx9s5ozJl9tN87yYHKDyVNja3mK3t4FN_PrGYHaXavQ';
 
@@ -10,7 +12,7 @@ const token =
 })
 export class AppComponent {
   title = 's3a-example';
-
+  items: Observable<any[]>;
   /**
    *
    */
@@ -22,21 +24,10 @@ export class AppComponent {
   getAll() {
     // Generate token example
     console.log('token');
-    this.httpclient
-      .get('localhost:12500', {
+    this.items = this.httpclient
+      .get<any[]>(`${environment.baseURL}WeatherForecast`, {
         headers: { Authorization: `Bearer ${token}` },
       })
-      .toPromise();
-  }
-
-  getbyId() {
-    // Generate token example
-
-    console.log('token');
-    this.httpclient
-      .get('localhost:12500/1', {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .toPromise();
+      .pipe();
   }
 }
